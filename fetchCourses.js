5823fc96f68225a4e6cd6a7b3c9daa86755f1032
sahana-gowda01 <sahana@navabharathtechnologies.com>
2026-05-15 16:11:37 +0530

    UI: Finalizing updates and syncing assets

---
+++ fetchCourses.js
@@ -0,0 +1,7 @@
+const http = require('http');
+
+http.get('http://192.168.1.15:5000/api/courses', (res) => {
+  let data = '';
+  res.on('data', chunk => data += chunk);
+  res.on('end', () => console.log(JSON.stringify(JSON.parse(data), null, 2)));
+});
